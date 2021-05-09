66eab6d124b7c0b674f45bac93dd8b02f29611a3
Gsandoval96 <gsandovalschmidt@gmail.com>
2021-05-09 12:37:38 +0200

    Añadida colisión fantasmas y refactorizas funciones de colisiones

--- pacman/src/MyGame.js
+++ pacman/src/MyGame.js
@@ -1,98 +1,104 @@
 class MyGame extends THREE.Object3D {
-  constructor(){
-    super();
+	constructor(){
+		super();
 
-	 this.start = false;
+		this.start = false;
 
-    // Title
+		// Title
 
-    var titlePos = new THREE.Vector3(-16, 24, 0);
-    this.title = new MyTitle(titlePos, 5, false);
-    this.add(this.title);
+		var titlePos = new THREE.Vector3(-16, 24, 0);
+		this.title = new MyTitle(titlePos, 5, false);
+		this.add(this.title);
 
-	 var redGhostDir = new THREE.Vector2(1,0);
-	 var pinkGhostDir = new THREE.Vector2(1,0);
-	 var blueGhostDir = new THREE.Vector2(1,0);
-	 var orangeGhostDir = new THREE.Vector2(0,-1);
+		var redGhostDir = new THREE.Vector2(1,0);
+		var pinkGhostDir = new THREE.Vector2(1,0);
+		var blueGhostDir = new THREE.Vector2(1,0);
+		var orangeGhostDir = new THREE.Vector2(0,-1);
 
-	 var pacmanPos = new THREE.Vector3(1, 0, 1);
-	 var redGhostPos = new THREE.Vector3(1, 0, 20);
-	 var pinkGhostPos = new THREE.Vector3(1, 0, 29);
-	 var blueGhostPos = new THREE.Vector3(6, 0, 26);
-	 var orangeGhostPos = new THREE.Vector3(21, 0, 1);
+		var pacmanPos = new THREE.Vector3(1, 0, 1);
+		var redGhostPos = new THREE.Vector3(1, 0, 20);
+		var pinkGhostPos = new THREE.Vector3(1, 0, 29);
+		var blueGhostPos = new THREE.Vector3(6, 0, 26);
+		var orangeGhostPos = new THREE.Vector3(21, 0, 1);
 
-	 this.pacman = new MyPacman(pacmanPos, MyConstant.CHARACTER_SIZE);
-	 this.redGhost = new MyGhost(redGhostPos, MyConstant.CHARACTER_SIZE, redGhostDir, MyMaterial.RED_GHOST);
-	 this.pinkGhost = new MyGhost(pinkGhostPos, MyConstant.CHARACTER_SIZE, pinkGhostDir, MyMaterial.PINK_GHOST);
-	 this.blueGhost = new MyGhost(blueGhostPos, MyConstant.CHARACTER_SIZE, blueGhostDir, MyMaterial.BLUE_GHOST);
-	 this.blueGhost.rotate("u");
-	 this.orangeGhost = new MyGhost(orangeGhostPos, MyConstant.CHARACTER_SIZE, orangeGhostDir, MyMaterial.ORANGE_GHOST);
-	 this.orangeGhost.rotate("d");
+		var pacman = new MyPacman(pacmanPos, MyConstant.CHARACTER_SIZE);
+		var redGhost = new MyGhost(redGhostPos, MyConstant.CHARACTER_SIZE, redGhostDir, MyMaterial.RED_GHOST);
+		var pinkGhost = new MyGhost(pinkGhostPos, MyConstant.CHARACTER_SIZE, pinkGhostDir, MyMaterial.PINK_GHOST);
+		var blueGhost = new MyGhost(blueGhostPos, MyConstant.CHARACTER_SIZE, blueGhostDir, MyMaterial.BLUE_GHOST);
+		blueGhost.rotate("u");
+		var orangeGhost = new MyGhost(orangeGhostPos, MyConstant.CHARACTER_SIZE, orangeGhostDir, MyMaterial.ORANGE_GHOST);
+		orangeGhost.rotate("d");
 
-	 this.add(this.pacman);
-	 this.add(this.redGhost);
-	 this.add(this.pinkGhost);
-	 this.add(this.blueGhost);
-	 this.add(this.orangeGhost);
+		// this.add(this.pacman);
+		// this.add(this.redGhost);
+		// this.add(this.pinkGhost);
+		// this.add(this.blueGhost);
+		// this.add(this.orangeGhost);
 
-	 this.maze = new MyMaze(MyConstant.BOX_SIZE);
-	 this.add(this.maze);
-  }
+		this.charaters = [];
+		this.charaters.push(pacman);
+		this.charaters.push(redGhost);
+		this.charaters.push(pinkGhost);
+		this.charaters.push(blueGhost);
+		this.charaters.push(orangeGhost);
 
-  startGame(){
-	  this.start = true;
-  }
+		for (var character of this.charaters) {
+			this.add(character);
+		}
 
-  collisionManager(){
-	  var last_pacmanPosX = this.pacman.getPosition().x;
-	  var last_pacmanPosZ = this.pacman.getPosition().z;
-	  var last_pacmanDir = new THREE.Vector2();
-	  last_pacmanDir.x = this.pacman.dirX;
-	  last_pacmanDir.y = this.pacman.dirZ;
+		this.maze = new MyMaze(MyConstant.BOX_SIZE);
+		this.add(this.maze);
+	}
 
-	  this.pacman.update();
-	  this.redGhost.update();
- 	 this.pinkGhost.update();
- 	 this.blueGhost.update();
- 	 this.orangeGhost.update();
+	startGame(){
+		this.start = true;
+	}
 
-	  var pacmanPosX = this.pacman.getPosition().x / MyConstant.BOX_SIZE;
-	  var pacmanPosZ = this.pacman.getPosition().z / MyConstant.BOX_SIZE;
-	  var pacmanDir = new THREE.Vector2();
-	  pacmanDir.x = this.pacman.dirX;
-	  pacmanDir.y = this.pacman.dirZ;
+	collisionManager(){
 
-	  if(pacmanDir.x == 1){// || (pacmanDir.x == 0 && last_pacmanDir.x == -1)){
-		  pacmanPosX = Math.ceil(pacmanPosX);
-	  }
-	  else if(pacmanDir.x == -1){
-	  	  pacmanPosX = Math.floor(pacmanPosX);
-	  }
-	  else{
-		  pacmanPosX = Math.round(pacmanPosX);
-	  }
+		for(let character of this.charaters){
+			let lastPos = new THREE.Vector2(character.getPosition().x, character.getPosition().z);
+			character.update();
+			let pos = new THREE.Vector2(character.getPosition().x / MyConstant.BOX_SIZE, character.getPosition().z / MyConstant.BOX_SIZE);
+			let dir = new THREE.Vector2(character.dirX, character.dirZ);
 
-	  if(pacmanDir.y == 1){
-		  pacmanPosZ = Math.ceil(pacmanPosZ);
-	  }
-	  else if(pacmanDir.y == -1){
-	  	  pacmanPosZ = Math.floor(pacmanPosZ);
-	  }
-	  else{
-		  pacmanPosZ = Math.round(pacmanPosZ);
-	  }
+			pos = this.adjustPosition(pos, dir);
 
-	  var collision = this.maze.checkCollision(this.pacman.getCollisionBox(), pacmanPosX, pacmanPosZ, pacmanDir);
+			if(this.maze.checkCollision(character.getCollisionBox(), pos, dir))
+			 	character.setPosition2D(lastPos);
+		}
+	}
 
+	adjustPosition(pos, dir){
 
-	  if(collision){
-		  this.pacman.setPosition2D(last_pacmanPosX, last_pacmanPosZ);
-	  }
-  }
+		var adjustedPosition = new THREE.Vector2(pos.x, pos.y);
 
-  update(){
+		if(dir.x == 1){
+			adjustedPosition.x = Math.ceil(pos.x);
+		}
+		else if(dir.x == -1){
+			adjustedPosition.x = Math.floor(pos.x);
+		}
+		else{
+			adjustedPosition.x = Math.round(pos.x);
+		}
+		if(dir.y == 1){
+			adjustedPosition.y = Math.ceil(pos.y);
+		}
+		else if(dir.y == -1){
+			adjustedPosition.y = Math.floor(pos.y);
+		}
+		else{
+			adjustedPosition.y = Math.round(pos.y);
+		}
+
+		return adjustedPosition;
+
+	}
+
+	update(){
 		if(this.start){
 			this.collisionManager();
 		}
-  }
+	}
 }
