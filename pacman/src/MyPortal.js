546142cf3ee36494aed5f784217b1465d4e63431
Gsandoval96 <gsandovalschmidt@gmail.com>
2021-11-17 03:23:50 +0100

    Cambiado modelo de los portales

--- pacman/src/MyPortal.js
+++ pacman/src/MyPortal.js
@@ -4,16 +4,17 @@ class MyPortal extends THREE.Object3D {
 
     // Creamos la geometría y la colocamos en el 0,0,0
     this.torusGeom = new THREE.TorusGeometry(size/3,size/20,20,50);
+
+    this.torusGeom.rotateY(Math.PI/2);
     this.texture = new THREE.TextureLoader().load('../img/portal.jpg');
     var material = new THREE.MeshBasicMaterial( { map: this.texture } );
 
     // Ya podemos construir el Mesh
     this.portal = new THREE.Mesh (this.torusGeom, material);
-    this.portal2 = new THREE.Mesh (this.torusGeom, material);
+    //this.portal.rotateZ(Math.PI/10);
 
     // Y añadirlo como hijo del Object3D
     this.add (this.portal);
-    this.add (this.portal2);
 
     this.sphereGeom = new THREE.SphereGeometry( size/10, 20.0, 20.0);
     this.dot = new THREE.Mesh (this.sphereGeom, material);
@@ -30,12 +31,6 @@ class MyPortal extends THREE.Object3D {
   }
 
   update(){
-    this.portal.rotation.y += 0.02;
-    this.portal.rotation.z += 0.025;
-    this.portal.rotation.x += 0.01;
-
-    this.portal2.rotation.y += 0.01;
-    this.portal2.rotation.z += 0.02;
-    this.portal2.rotation.x += 0.025;
+    this.portal.rotation.x += 0.05;
   }
 }
